404eefc2177dbfe9e223ce3895e3f6b9a0597eec
Hiroki Kondo <kondo.matr02@gmail.com>
2023-01-09 17:11:54 +0900

    OwnerSettingOperatingTimeViewを実装

---
+++ OwnerCommunicationHelperApp/View/OwnerSettingOperatingTimeView.swift
@@ -0,0 +1,31 @@
+//
+//  OwnerSettingOperatingTimeView.swift
+//  OwnerCommunicationHelperApp
+//
+//  Created by 近藤宏輝 on 2023/01/09.
+//
+
+import SwiftUI
+
+struct OwnerSettingOperatingTimeView: View {
+    var startTime: String
+    var endTime: String
+
+    var body: some View {
+        ZStack {
+            PrimaryColor.buttonColor
+            VStack {
+                CommonText(text: "支援者", alignment: .leading)
+                CommonText(text: "開始　" + startTime, alignment: .trailing)
+                CommonText(text: "終了 " + endTime, alignment: .trailing)
+            }
+            .padding(20)
+        }
+    }
+}
+
+struct OwnerSettingOperatingTimeView_Previews: PreviewProvider {
+    static var previews: some View {
+        OwnerSettingOperatingTimeView(startTime: "", endTime: "")
+    }
+}
