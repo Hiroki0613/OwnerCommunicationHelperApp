0aa693bc8de72912d053bf603cc0d581ae31c3a2
Hiroki Kondo <kondo.matr02@gmail.com>
2023-01-09 22:47:31 +0900

    Authを実装

--- OwnerCommunicationHelperApp/OwnerCommunicationHelperAppApp.swift
+++ OwnerCommunicationHelperApp/OwnerCommunicationHelperAppApp.swift
@@ -9,12 +9,18 @@ import ComposableArchitecture
 import Firebase
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
 struct OwnerCommunicationHelperAppApp: App {
-
-    init() {
-        FirebaseApp.configure()
-    }
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
