8ff09217327c1b40c941c9c9203ec04e4e92262a
Hiroki Kondo <kondo.matr02@gmail.com>
2023-01-09 17:20:28 +0900

    OwnerSettingSubscriptionViewを実装

---
+++ OwnerCommunicationHelperApp/View/OwnerSettingSubscriptionView.swift
@@ -0,0 +1,28 @@
+//
+//  OwnerSettingSubscriptionView.swift
+//  OwnerCommunicationHelperApp
+//
+//  Created by 近藤宏輝 on 2023/01/09.
+//
+
+import SwiftUI
+
+struct OwnerSettingSubscriptionView: View {
+    var body: some View {
+        ZStack {
+            PrimaryColor.buttonColor
+            VStack {
+                CommonText(text: "運用状況", alignment: .leading)
+                CommonText(text: "可能登録人数 12人", alignment: .trailing)
+                CommonText(text: "残り 7人", alignment: .trailing)
+            }
+            .padding(20)
+        }
+    }
+}
+
+struct OwnerSettingsubscriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        OwnerSettingSubscriptionView()
+    }
+}
