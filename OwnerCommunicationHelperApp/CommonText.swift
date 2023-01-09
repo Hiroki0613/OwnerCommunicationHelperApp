5e14f0e5b9dd50d3663f2fd5c239ee212dcf602f
Hiroki Kondo <kondo.matr02@gmail.com>
2023-01-09 17:11:28 +0900

    CommonTextを実装

---
+++ OwnerCommunicationHelperApp/CommonText.swift
@@ -0,0 +1,21 @@
+//
+//  CommonText.swift
+//  OwnerCommunicationHelperApp
+//
+//  Created by 近藤宏輝 on 2023/01/09.
+//
+
+import SwiftUI
+
+struct CommonText: View{
+    var text: String
+    var alignment: TextAlignment
+
+    var body some View {
+        Text(text)
+            .fontWeight(.semibold)
+            .frame(maxWidth: .infinity, alignment: alignment)
+            .font(.system(size: 20))
+            .foregroundColor(Color.black)
+    }
+}
