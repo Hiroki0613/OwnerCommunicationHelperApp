e744e5733a7599ce3912ce66bbbfea609aaf48a6
Hiroki Kondo <kondo.matr02@gmail.com>
2023-02-07 22:03:21 +0900

    コメントを追加

--- OwnerCommunicationHelperApp/FunctionalParts/Chat/Message.swift
+++ OwnerCommunicationHelperApp/FunctionalParts/Chat/Message.swift
@@ -12,6 +12,7 @@ struct Message: Identifiable, Codable {
     var personalId: String
     // ここに身体情報を入れる
     // Owner側も入れること
+    // 他に情報が入っていたら追加する。
     /*
      支援者の方も脈拍測定は必要です。
      理由:障害当事者が支援者側の生態情報を見て相手の感情を読み取るため。
