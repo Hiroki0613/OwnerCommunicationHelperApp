997fc19a6523373f6eb2f0f533aad288eb1b112f
Hiroki Kondo <kondo.matr02@gmail.com>
2023-03-23 21:54:54 +0900

    コメントを追加

--- OwnerCommunicationHelperApp/View/Setting/OwnerSettingTopView.swift
+++ OwnerCommunicationHelperApp/View/Setting/OwnerSettingTopView.swift
@@ -36,7 +36,7 @@ struct OwnerSettingTopView: View {
                             OwnerSettingPressureView(viewStore: viewStore)
                                 .cornerRadius(20)
                             Spacer().frame(height: 30)
-                            // TODO: 可能参加人数をデフォルトで決めておく。
+                            // TODO: Artcol相談 可能参加人数をデフォルトで決めておく。
                             // TODO: 残り人数はFirebaseと連携させておく。Workerの数を読み出してカウントに入れるようにする。
                             // TODO: 課金方法については、後から検討する。ここで可能参加人数を変更できるようにする。
                             OwnerSettingSubscriptionView(numberOfPeopleCanRegister: ownerSettingManager.owner.numberOfPeopleCanRegister)
