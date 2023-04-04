da3ed3e452fde57f19be930ceb7101c3f78037eb
Hiroki Kondo <kondo.matr02@gmail.com>
2023-04-04 22:57:55 +0900

    OwnerRegisterDeviceViewを追加

---
+++ OwnerCommunicationHelperApp/View/OwnerRegisterDeviceView.swift
@@ -0,0 +1,20 @@
+//
+//  OwnerRegisterDeviceView.swift
+//  OwnerCommunicationHelperApp
+//
+//  Created by 近藤宏輝 on 2023/04/04.
+//
+
+import SwiftUI
+
+struct OwnerRegisterDeviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OwnerRegisterDeviceView_Previews: PreviewProvider {
+    static var previews: some View {
+        OwnerRegisterDeviceView()
+    }
+}
