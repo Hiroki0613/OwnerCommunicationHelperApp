4ce0e0b6c4d0799dd3e05a9eb286e72b0e27bbbb
Hiroki Kondo <kondo.matr02@gmail.com>
2023-04-10 23:49:07 +0900

    OwnerSelectRegisterViewを実装

---
+++ OwnerCommunicationHelperApp/View/OwnerSelectRegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  OwnerSelectRegisterView.swift
+//  OwnerCommunicationHelperApp
+//
+//  Created by 近藤宏輝 on 2023/04/10.
+//
+
+import SwiftUI
+
+struct OwnerSelectRegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OwnerSelectRegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        OwnerSelectRegisterView()
+    }
+}
