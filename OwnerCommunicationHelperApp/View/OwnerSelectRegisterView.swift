10c492fc028c7f54fb994dcfc5020ff1dabe3c18
Hiroki Kondo <kondo.matr02@gmail.com>
2023-04-11 00:02:42 +0900

    OwnerSelectRegisterViewを選択制にする。

--- OwnerCommunicationHelperApp/View/OwnerSelectRegisterView.swift
+++ OwnerCommunicationHelperApp/View/OwnerSelectRegisterView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct OwnerSelectRegisterView: View {
+    // 登録する対処が、デバイス、作業者、スタッフを選ぶ
+    var registerArray = ["デバイス", "作業者", "スタッフ"]
+
+    init() {
+        UITableView.appearance().backgroundColor = PrimaryUIColor.background
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            PrimaryColor.backgroundGreen
+                .ignoresSafeArea()
+            ScrollView {
+                VStack {
+                    Spacer().frame(height: 20)
+                    Text("スタッフ")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 20))
+                        .foregroundColor(Color.black)
+                    ForEach(0..<registerArray.count) { index in
+                        switch index {
+                        case 0:
+                            OwnerManageStaffCellView(name: registerArray[index])
+                                .cornerRadius(20)
+                        case 1:
+                            OwnerManageStaffCellView(name: registerArray[index])
+                                .cornerRadius(20)
+                        case 2:
+                            OwnerManageStaffCellView(name: registerArray[index])
+                                .cornerRadius(20)
+                        default:
+                            OwnerManageStaffCellView(name: registerArray[index])
+                                .cornerRadius(20)
+                        }
+                        Spacer().frame(height: 10)
+                    }
+                }
+                .padding(.horizontal, 30)
+            }
+            .clipped()
+        }
     }
 }
 
