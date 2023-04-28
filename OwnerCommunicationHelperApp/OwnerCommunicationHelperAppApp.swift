5336fdcee74163408955bbbe29a3fb22022a9484
Hiroki Kondo <kondo.matr02@gmail.com>
2023-04-28 12:31:44 +0900

    コメントを追加

--- OwnerCommunicationHelperApp/OwnerCommunicationHelperAppApp.swift
+++ OwnerCommunicationHelperApp/OwnerCommunicationHelperAppApp.swift
@@ -13,6 +13,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        // TODO: ここでPush通知のコードを書くこと。
         return true
     }
 }
